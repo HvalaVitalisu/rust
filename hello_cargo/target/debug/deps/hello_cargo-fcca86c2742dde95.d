45675e55f5e3ea17250afdbd1d1c1a15ebcd308a
HvalaVitalisu <hvalavitalisu@gmail.com>
2022-12-24 02:32:34 +0100

    first

---
+++ hello_cargo/target/debug/deps/hello_cargo-fcca86c2742dde95.d
@@ -0,0 +1,5 @@
+/home/vitalis/git/rust/hello_cargo/target/debug/deps/hello_cargo-fcca86c2742dde95: src/main.rs
+
+/home/vitalis/git/rust/hello_cargo/target/debug/deps/hello_cargo-fcca86c2742dde95.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/vitalis/git/rust/hello_cargo/target/debug/hello_cargo: /home/vitalis/git/rust/hello_cargo/src/main.rs
